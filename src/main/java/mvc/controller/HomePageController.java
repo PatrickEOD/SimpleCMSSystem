a64741643dbb1a762751fbcb62f5a1278a9757a5
PatrickEOD <patrykkogorek@gmail.com>
2018-07-05 22:44:15 +0200

    Modyfication: changing from Dao to repository for HomePageController

--- src/main/java/mvc/controller/HomePageController.java
+++ src/main/java/mvc/controller/HomePageController.java
@@ -8,12 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mvc.dao.ArticleDao;
 import mvc.dao.CategoryDao;
 import mvc.entity.Category;
+import mvc.repositories.ArticleRepository;
+import mvc.repositories.CategoryRepository;
 
 @Controller
 @RequestMapping("/home")
@@ -21,21 +22,27 @@ public class HomePageController {
 	
 	private final ArticleDao articleDao;
 	private final CategoryDao categoryDao;
+	private final ArticleRepository articleRepository;
+	private final CategoryRepository categoryRepository;
 	
 	@Autowired
-	public HomePageController(ArticleDao articleDao, CategoryDao categoryDao) {
+	public HomePageController(ArticleDao articleDao, CategoryDao categoryDao, ArticleRepository articleRepository, CategoryRepository categoryRepository) {
 		this.articleDao = articleDao;
 		this.categoryDao = categoryDao;
+		this.articleRepository = articleRepository;
+		this.categoryRepository = categoryRepository;
 	}
 	
 	@ModelAttribute("categories")
 	public List<Category> getCategories(){
-		return categoryDao.readAllCategories();
+//		return categoryDao.readAllCategories();
+		return categoryRepository.findAll();
 	}
 
 	@GetMapping("/")
 	public String getLastFive(Model model) {
-		model.addAttribute("listLast5", articleDao.getLastFiveArticles());
+//		model.addAttribute("listLast5", articleDao.getLastFiveArticles());
+		model.addAttribute("listLast5", articleRepository.findFirst5ByDraftOrderByCreatedAsc(false));
 		return "homePage/homePage";
 	}
 	
@@ -46,7 +53,8 @@ public class HomePageController {
 	
 	@GetMapping("/listByCategories/{id}")
 	public String getListByCategories(Model model, @PathVariable Long id) {
-		model.addAttribute("categoryList", articleDao.getListByCategory(id));
+//		model.addAttribute("categoryList", articleDao.getListByCategory(id));
+		model.addAttribute("categoryList", articleRepository.findByCategoryId(id));
 		return "homePage/listByCategories";
 	}
 }
--- src/main/java/mvc/repositories/ArticleRepository.java
+++ src/main/java/mvc/repositories/ArticleRepository.java
@@ -1,11 +1,18 @@
 package mvc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import mvc.entity.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+	
+	List<Article> findFirst5ByDraftOrderByCreatedAsc(boolean draft);
+	
+	List<Article> findByCategoryId(Long id);
 }
